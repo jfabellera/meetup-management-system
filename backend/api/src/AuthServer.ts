57758b57e0238b177b2b6b308cfe6fb741ae437a
Jan-Felix Abellera <jfabellera@gmail.com>
2023-09-15 00:28:06 -0500

    Add AuthServer class
    No specific endpoints at the moment

---
+++ backend/api/src/AuthServer.ts
@@ -0,0 +1,49 @@
+import * as express from 'express'
+
+const DEFAULT_AUTH_SERVER_PORT = '3001'
+
+class AuthServer {
+  private express: express.Application
+
+  constructor() {
+    this.express = express.default()
+    this.config()
+    this.routes()
+  }
+
+  private config(): void {
+    this.express.use(express.json())
+    this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(function (req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers')
+      next()
+    })
+  }
+
+  private routes(): void {
+    this.express.get('/', (req, res, next) => {
+      res.send("Meetup Management System Authentication")
+    })
+
+    this.express.use("*", (req, res, next) => {
+      res.send("Not a valid endpoint.")
+    })
+  }
+
+  public start = (port: number) => {
+    return new Promise((resolve, reject) => {
+      this.express.listen(port, () => {
+        resolve(port)
+      }).on('error', (err: Object) => reject(err))
+    })
+  }
+}
+
+const port = parseInt(process.env.MMS_AUTH_SERVER_PORT || DEFAULT_AUTH_SERVER_PORT)
+const server = new AuthServer().start(port)
+  .then(port => console.log(`Running on port ${port}`))
+  .catch(error => {
+    console.log(error)
+  })
