259420b5d7425052e7a646e7949466a0a94d14d7
Jan-Felix Abellera <jfabellera@gmail.com>
2023-11-02 22:50:30 -0500

    Move createTicket endpoint to meetup router

--- backend/api/src/routes/meetups.ts
+++ backend/api/src/routes/meetups.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { getAllMeetups, getMeetup, createMeetup, updateMeetup, deleteMeetup } from '../controllers/meetups'
+import { createTicket } from '../controllers/tickets';
 import { authChecker, Rule } from '../middleware/authChecker';
 
 const router = express.Router();
@@ -9,5 +10,6 @@ router.get('/:meetup_id', getMeetup);
 router.post('/', authChecker([Rule.requireOrganizer]), createMeetup);
 router.put('/:meetup_id', authChecker([Rule.requireOrganizer]), updateMeetup);
 router.delete('/:meetup_id', authChecker([Rule.requireOrganizer]), deleteMeetup);
+router.post('/:meetup_id/rsvp', authChecker(), createTicket);
 
 export default router;
