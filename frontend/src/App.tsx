3d64d559773c321a95c3f3bece1f6ec8ebb37d89
Jan-Felix Abellera <jfabellera@gmail.com>
2023-11-08 23:04:44 -0600

    Add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/Homepage';
+import LoginPage from './pages/LoginPage';
 
 import { store } from './store/store';
 import { Provider } from 'react-redux';
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
     </Provider>
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,54 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import Page from '../components/Page/Page';
+
+const LoginPage = (): JSX.Element => {
+  return (
+    <Page>
+      <Flex align={'center'} justify={'center'}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Sign in</Heading>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}
+          >
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" />
+              </FormControl>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}
+              >
+                Sign in
+              </Button>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    </Page>
+  );
+};
+
+export default LoginPage;
