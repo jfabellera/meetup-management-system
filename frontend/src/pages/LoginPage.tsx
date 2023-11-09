aaee9aee95ef26ea199658b617003e94d3734317
Jan-Felix Abellera <jfabellera@gmail.com>
2023-11-08 23:47:37 -0600

    Add login page state management

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -9,9 +9,33 @@ import {
   Stack,
   useColorModeValue,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 import Page from '../components/Page/Page';
 
+interface Login {
+  email: string;
+  password: string;
+}
+
 const LoginPage = (): JSX.Element => {
+  const [login, setLogin] = useState<Login>({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setLogin({
+      ...login,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+
+    // TODO(jan): add login api call or dispatch something something redux here
+  };
+
   return (
     <Page>
       <Flex align={'center'} justify={'center'}>
@@ -25,25 +49,32 @@ const LoginPage = (): JSX.Element => {
             boxShadow={'lg'}
             p={8}
           >
-            <Stack spacing={4}>
-              <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>Password</FormLabel>
-                <Input type="password" />
-              </FormControl>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}
-              >
-                Sign in
-              </Button>
-            </Stack>
+            <form onSubmit={handleSubmit}>
+              <Stack spacing={4}>
+                <FormControl id="email">
+                  <FormLabel>Email address</FormLabel>
+                  <Input type="email" name="email" onChange={handleChange} />
+                </FormControl>
+                <FormControl id="password">
+                  <FormLabel>Password</FormLabel>
+                  <Input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                  />
+                </FormControl>
+                <Button
+                  type="submit"
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}
+                >
+                  Sign in
+                </Button>
+              </Stack>
+            </form>
           </Box>
         </Stack>
       </Flex>
