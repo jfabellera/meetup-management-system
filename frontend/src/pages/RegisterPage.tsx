9e20b229293d5594b524659119f4b8049465fca0
Jan-Felix Abellera <jfabellera@gmail.com>
2023-11-20 00:42:30 -0600

    Add form validation
    Missing password requirements

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -6,6 +6,7 @@ import {
   Checkbox,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   HStack,
@@ -14,58 +15,61 @@ import {
   Stack,
   Text,
   useColorModeValue,
+  type BoxProps,
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useFormik } from 'formik';
 import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
 import Page from '../components/Page/Page';
-import { register, type RegisterPayload } from '../store/authSlice';
+import { register } from '../store/authSlice';
 import { useAppDispatch } from '../store/hooks';
 
+const RegisterSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  firstName: Yup.string().required('Required'),
+  lastName: Yup.string().required('Required'),
+  nickName: Yup.string().required('Required'),
+  password: Yup.string().required('Required'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password')], 'Passwords must match')
+    .required('Required'),
+});
+
 const RegisterPage = (): JSX.Element => {
-  const [registerPayload, setRegisterPayload] = useState<RegisterPayload>({
-    email: '',
-    firstName: '',
-    lastName: '',
-    nickName: '',
-    password: '',
-  });
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      nickName: '',
+      password: '',
+      confirmPassword: '',
+    },
+    onSubmit: (values) => {
+      dispatch(register(values))
+        .then((action) => {
+          // Get status of register
+          if (register.fulfilled.match(action)) {
+            // Successfully registered, redirect user to login page
+            navigate('/login');
+          } else if (register.rejected.match(action)) {
+            // Failed to register, show an error message
+            // TODO(jan)
+          }
+        })
+        .catch(() => {});
+    },
+    validationSchema: RegisterSchema,
+  });
 
-  /**
-   * Handle form input changes. This updates the register payload to be
-   * dispatched.
-   *
-   * @param event
-   */
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setRegisterPayload({
-      ...registerPayload,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  /**
-   * Handle form submit. This dispatches the register action using the register
-   * payload.
-   *
-   * @param event
-   */
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-
-    dispatch(register(registerPayload))
-      .then((action) => {
-        // Get status of register
-        if (register.fulfilled.match(action)) {
-          // Successfully registered, redirect user to login page
-          navigate('/login');
-        } else if (register.rejected.match(action)) {
-          // Failed to register, show an error message
-          // TODO(jan)
-        }
-      })
-      .catch(() => {});
+  const ErrorMessage = ({ children }: BoxProps): JSX.Element => {
+    return (
+      <FormErrorMessage position="absolute" right="0" margin="1">
+        {children}
+      </FormErrorMessage>
+    );
   };
 
   return (
@@ -83,52 +87,115 @@ const RegisterPage = (): JSX.Element => {
             boxShadow={'lg'}
             p={8}
           >
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={formik.handleSubmit} noValidate>
               <Stack spacing={4}>
                 <HStack>
                   <Box>
-                    <FormControl id="firstName" isRequired>
+                    <FormControl
+                      id="firstName"
+                      isRequired
+                      isInvalid={
+                        formik.errors.firstName != null &&
+                        formik.touched.firstName
+                      }
+                    >
                       <FormLabel>First Name</FormLabel>
                       <Input
                         type="text"
                         name="firstName"
-                        onChange={handleChange}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                       />
+                      <ErrorMessage>{formik.errors.firstName}</ErrorMessage>
                     </FormControl>
                   </Box>
                   <Box>
-                    <FormControl id="lastName" isRequired>
+                    <FormControl
+                      id="lastName"
+                      isRequired
+                      isInvalid={
+                        formik.errors.lastName != null &&
+                        formik.touched.lastName
+                      }
+                    >
                       <FormLabel>Last Name</FormLabel>
                       <Input
                         type="text"
                         name="lastName"
-                        onChange={handleChange}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                       />
+                      <ErrorMessage>{formik.errors.lastName}</ErrorMessage>
                     </FormControl>
                   </Box>
                 </HStack>
-                <FormControl id="nickName" isRequired>
+                <FormControl
+                  id="nickName"
+                  isRequired
+                  isInvalid={
+                    formik.errors.nickName != null && formik.touched.nickName
+                  }
+                >
                   <FormLabel>Display Name</FormLabel>
-                  <Input type="text" name="nickName" onChange={handleChange} />
+                  <Input
+                    type="text"
+                    name="nickName"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  <ErrorMessage>{formik.errors.nickName}</ErrorMessage>
                 </FormControl>
-                <FormControl id="email" isRequired>
+                <FormControl
+                  id="email"
+                  isRequired
+                  isInvalid={
+                    formik.errors.email != null && formik.touched.email
+                  }
+                >
                   <FormLabel>Email address</FormLabel>
-                  <Input type="email" name="email" onChange={handleChange} />
+                  <Input
+                    type="email"
+                    name="email"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  <ErrorMessage>{formik.errors.email}</ErrorMessage>
                 </FormControl>
-                <FormControl id="password" isRequired>
+                <FormControl
+                  id="password"
+                  isRequired
+                  isInvalid={
+                    formik.errors.password != null && formik.touched.password
+                  }
+                >
                   <FormLabel>Password</FormLabel>
                   <Input
                     type="password"
                     name="password"
-                    onChange={handleChange}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                   />
+                  <ErrorMessage>{formik.errors.password}</ErrorMessage>
                 </FormControl>
-                <FormControl id="confirmPassword" isRequired>
+                <FormControl
+                  id="confirmPassword"
+                  isRequired
+                  isInvalid={
+                    formik.errors.confirmPassword != null &&
+                    formik.touched.confirmPassword
+                  }
+                >
                   <FormLabel>Confirm Password</FormLabel>
-                  <Input type="password" name="confirmPassword" />
+                  <Input
+                    type="password"
+                    name="confirmPassword"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  <ErrorMessage>{formik.errors.confirmPassword}</ErrorMessage>
                 </FormControl>
                 <FormControl id="requestOrganizer">
-                  <Flex justify={'center'}>
+                  <Flex justify={'center'} mt="2">
                     <Stack direction="row">
                       <FormLabel margin={0} pr="4">
                         Are you an organizer?
