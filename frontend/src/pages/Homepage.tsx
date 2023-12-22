e46af2a334fa1c0af403d0b9dd8fe563673f293b
Jan-Felix Abellera <jfabellera@gmail.com>
2023-12-22 15:05:55 -0700

    Make meetup modal look better

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -7,10 +7,11 @@ import {
   Grid,
   GridItem,
   Heading,
+  HStack,
+  Icon,
   Image,
   Modal,
   ModalBody,
-  ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalHeader,
@@ -22,6 +23,7 @@ import {
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import { useState } from 'react';
+import { FiCalendar, FiClock, FiMapPin, FiUser } from 'react-icons/fi';
 import Page from '../components/Page/Page';
 import { useGetMeetupQuery, useGetMeetupsQuery } from '../store/meetupSlice';
 
@@ -64,33 +66,84 @@ const Homepage = (): JSX.Element => {
       ) : (
         <></>
       )}
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        motionPreset={'slideInBottom'}
+        size={'3xl'}
+        scrollBehavior={'inside'}
+      >
         <ModalOverlay />
+
         <ModalContent>
-          <ModalHeader>Meetup Details</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Flex h={32} w="full">
-              <Image
-                w="full"
-                src="https://cdn.discordapp.com/attachments/1089785393014112278/1158544783527129169/image.png?ex=651d4b29&is=651bf9a9&hm=c95e6cf90f1996ed9626799d43cfb55f693a8f55a2be58ddf14dd0a7c95c5324&"
-                alt="testing this shit outs"
-                borderRadius="lg"
-                objectFit="cover"
-              />
-            </Flex>
+          <Image
+            src="https://media.discordapp.net/attachments/1149502169041621062/1182553630407135292/Eventbrite.jpg?ex=65851de4&is=6572a8e4&hm=bdc554f39abecd6436f4f344518f68c845a4340da137ad76ebfc258c034464cc&=&format=webp&width=2592&height=1296"
+            borderRadius={'md'}
+          />
+          <ModalHeader fontWeight={'semibold'} paddingBottom={'0.4em'}>
             <Heading>{meetup?.name}</Heading>
+          </ModalHeader>
+          <ModalBody>
+            <Flex
+              direction={'column'}
+              paddingBottom={'1em'}
+              fontWeight={'semibold'}
+            >
+              {/* Date */}
+              <HStack>
+                <Icon as={FiCalendar} />
+                <Text>
+                  {dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format(
+                    'MMMM DD, YYYY',
+                  )}
+                </Text>
+              </HStack>
+
+              {/* Time */}
+              {/* TODO(jan): Add end time once implemented into API */}
+              <HStack>
+                <Icon as={FiClock} />
+                <Text>
+                  {dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format('h:mm A')}
+                </Text>
+              </HStack>
+
+              {/* Location */}
+              <HStack>
+                <Icon as={FiMapPin} />
+                <Text>{meetup?.location.full_address}</Text>
+              </HStack>
+
+              {/* Organizers */}
+              <HStack>
+                <Icon as={FiUser} />
+                <Text>
+                  Organized by{' '}
+                  {new Intl.ListFormat().format(meetup?.organizers)}
+                </Text>
+              </HStack>
+            </Flex>
+
+            {/* Description */}
+            {/* TODO(jan): Add actual description from meetup details when implemented */}
             <Text>
-              {dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format(
-                'MMMM DD, YYYY @ h:mm A',
-              )}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Text>
-            <Text>{meetup?.location.full_address}</Text>
-            <Text>Organizers: {meetup?.organizers.join(', ')}</Text>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            {/* TODO(jan): Implement */}
+            <Button colorScheme={'green'} mr={3}>
+              RSVP
+            </Button>
+            <Button colorScheme={'gray'} mr={3} onClick={onClose}>
               Close
             </Button>
           </ModalFooter>
