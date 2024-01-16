f0d72d6719728827701663892b991912b2a0c618
Jan-Felix Abellera <jfabellera@gmail.com>
2024-01-15 20:29:00 -0600

    Fix linting errors

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -148,119 +148,122 @@ const Homepage = (): JSX.Element => {
       ) : (
         <></>
       )}
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        isCentered
-        motionPreset={'slideInBottom'}
-        size={'3xl'}
-        scrollBehavior={'inside'}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalBody padding={0}>
-            {meetup?.image_url != null ? (
-              <AspectRatio ratio={2 / 1}>
-                <Image
-                  src={meetup?.image_url}
-                  fallbackSrc="https://via.placeholder.com/150"
-                  objectFit="cover"
-                  borderTopRadius={'md'}
-                />
-              </AspectRatio>
-            ) : null}
-            <Flex direction={'column'} padding={'1em'} paddingBottom={'0'}>
-              <Heading paddingBottom={'0.4em'}>{meetup?.name}</Heading>
-              <Flex
-                direction={'column'}
-                paddingBottom={'1em'}
-                fontWeight={'semibold'}
-              >
-                {/* Date */}
-                <HStack>
-                  <Icon as={FiCalendar} />
-                  <Text>
-                    {dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format(
-                      'MMMM DD, YYYY',
-                    )}
-                  </Text>
-                </HStack>
+      {meetup != null ? (
+        <Modal
+          isOpen={isOpen}
+          onClose={onClose}
+          isCentered
+          motionPreset={'slideInBottom'}
+          size={'3xl'}
+          scrollBehavior={'inside'}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalBody padding={0}>
+              {meetup.image_url != null ? (
+                <AspectRatio ratio={2 / 1}>
+                  <Image
+                    src={meetup.image_url}
+                    fallbackSrc="https://via.placeholder.com/150"
+                    objectFit="cover"
+                    borderTopRadius={'md'}
+                  />
+                </AspectRatio>
+              ) : null}
+              <Flex direction={'column'} padding={'1em'} paddingBottom={'0'}>
+                <Heading paddingBottom={'0.4em'}>{meetup.name}</Heading>
+                <Flex
+                  direction={'column'}
+                  paddingBottom={'1em'}
+                  fontWeight={'semibold'}
+                >
+                  {/* Date */}
+                  <HStack>
+                    <Icon as={FiCalendar} />
+                    <Text>
+                      {dayjs(meetup.date, 'YYYY-MM-DDTHH:mm:ss').format(
+                        'MMMM DD, YYYY',
+                      )}
+                    </Text>
+                  </HStack>
 
-                {/* Time */}
-                {/* TODO(jan): Add end time once implemented into API */}
-                <HStack>
-                  <Icon as={FiClock} />
-                  <Text>
-                    {dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format(
-                      'h:mm A',
-                    )}
-                  </Text>
-                </HStack>
+                  {/* Time */}
+                  {/* TODO(jan): Add end time once implemented into API */}
+                  <HStack>
+                    <Icon as={FiClock} />
+                    <Text>
+                      {dayjs(meetup.date, 'YYYY-MM-DDTHH:mm:ss').format(
+                        'h:mm A',
+                      )}
+                    </Text>
+                  </HStack>
 
-                {/* Location */}
-                <HStack>
-                  <Icon as={FiMapPin} />
-                  <Text>{meetup?.location.full_address}</Text>
-                </HStack>
+                  {/* Location */}
+                  <HStack>
+                    <Icon as={FiMapPin} />
+                    <Text>{meetup.location.full_address}</Text>
+                  </HStack>
 
-                {/* Organizers */}
-                <HStack>
-                  <Icon as={FiUser} />
-                  <Text>
-                    Organized by{' '}
-                    {new Intl.ListFormat().format(meetup?.organizers)}
-                  </Text>
-                </HStack>
+                  {/* Organizers */}
+                  <HStack>
+                    <Icon as={FiUser} />
+                    <Text>
+                      Organized by{' '}
+                      {new Intl.ListFormat().format(meetup.organizers)}
+                    </Text>
+                  </HStack>
+                </Flex>
+
+                {/* Description */}
+                {/* TODO(jan): Add actual description from meetup details when implemented */}
+                <Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Text>
               </Flex>
+            </ModalBody>
 
-              {/* Description */}
-              {/* TODO(jan): Add actual description from meetup details when implemented */}
-              <Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Text>
-            </Flex>
-          </ModalBody>
-
-          <ModalFooter>
-            <MeetupCapacityStatus
-              available={meetup?.tickets.available}
-              total={meetup?.tickets.total}
-            />
-            <Spacer />
-            {/* TODO(jan): Implement */}
-            {isAttendingMeetup(meetup?.id) ? (
-              <Button
-                leftIcon={<FiUserX />}
-                colorScheme={'red'}
-                mr={3}
-                disabled={!isLoggedIn}
-                onClick={unrsvpOnClick}
-              >
-                Cancel RSVP
+            <ModalFooter>
+              <MeetupCapacityStatus
+                available={meetup.tickets.available}
+                total={meetup.tickets.total}
+              />
+              <Spacer />
+              {/* TODO(jan): Implement */}
+              {isAttendingMeetup(meetup.id) ? (
+                <Button
+                  leftIcon={<FiUserX />}
+                  colorScheme={'red'}
+                  mr={3}
+                  disabled={!isLoggedIn}
+                  onClick={unrsvpOnClick}
+                >
+                  Cancel RSVP
+                </Button>
+              ) : (
+                <Button
+                  leftIcon={<FiUserCheck />}
+                  colorScheme={'green'}
+                  mr={3}
+                  disabled={!isLoggedIn}
+                  onClick={rsvpOnclick}
+                >
+                  RSVP
+                </Button>
+              )}
+              <Button colorScheme={'pink'} mr={3} onClick={onClose}>
+                Close
               </Button>
-            ) : (
-              <Button
-                leftIcon={<FiUserCheck />}
-                colorScheme={'green'}
-                mr={3}
-                disabled={!isLoggedIn}
-                onClick={rsvpOnclick}
-              >
-                RSVP
-              </Button>
-            )}
-            <Button colorScheme={'pink'} mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      ) : null}
     </Page>
   );
 };
