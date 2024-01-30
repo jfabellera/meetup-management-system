bdbeb8dbd52c80897be0c67ea3986e4b118f6f75
Jan-Felix Abellera <jfabellera@gmail.com>
2024-01-29 23:28:06 -0600

    Add heading and "new meetup" button

--- frontend/src/pages/OrganizerDashboard.tsx
+++ frontend/src/pages/OrganizerDashboard.tsx
@@ -1,9 +1,16 @@
+import { Button, Container, Heading, HStack, Spacer } from '@chakra-ui/react';
 import Page from '../components/Page/Page';
 
 const OrganizerDashboard = (): JSX.Element => {
   return (
     <Page>
-      <></>
+      <Container padding={0}>
+        <HStack>
+          <Heading fontWeight={'semibold'}>Your Meetups</Heading>
+          <Spacer />
+          <Button colorScheme={'green'}>New meetup</Button>
+        </HStack>
+      </Container>
     </Page>
   );
 };
