4c34062e9a3da4d31fd3dcccae6bec09442caedf
Jan-Felix Abellera <jfabellera@gmail.com>
2024-02-04 16:00:09 -0600

    Refactor getAllMeetups and getMeetup to use a single interface

--- frontend/src/store/meetupSlice.ts
+++ frontend/src/store/meetupSlice.ts
@@ -1,8 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type {
-  FullMeetupInfo,
-  SimpleMeetupInfo,
-} from '../../../backend/src/controllers/meetups';
+import type { MeetupInfo } from '../../../backend/src/controllers/meetups';
 import config from '../config';
 
 export interface GetMeetupsOptions {
@@ -16,14 +13,14 @@ export const meetupSlice = createApi({
     baseUrl: `${config.apiUrl}:${config.apiPort}/`,
   }),
   endpoints: (builder) => ({
-    getMeetups: builder.query<SimpleMeetupInfo[], GetMeetupsOptions>({
+    getMeetups: builder.query<MeetupInfo[], GetMeetupsOptions>({
       query: (options) => ({
         url: `meetups/`,
         params: options,
       }),
       providesTags: ['Meetups'],
     }),
-    getMeetup: builder.query<FullMeetupInfo, number>({
+    getMeetup: builder.query<MeetupInfo, number>({
       query: (id) => ({
         url: `meetups/${id}`,
       }),
