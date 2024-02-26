9e7d85ffbf348fcfabad996bf976b8de1962b9cb
Jan-Felix Abellera <jfabellera@gmail.com>
2024-02-25 22:04:04 -0600

    Move meetup form schema to its own file

---
+++ frontend/src/util/schemas/MeetupFormSchema.ts
@@ -0,0 +1,21 @@
+import * as Yup from 'yup';
+
+const MeetupFormSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, 'Name must be at least 3 characters')
+    .required('Required'),
+  date: Yup.date()
+    .min(new Date(), 'Date must be in the future')
+    .required('Required'),
+  startTime: Yup.string().required('Required'),
+  address: Yup.string().required('Required'),
+  duration: Yup.number()
+    .moreThan(0, 'Must be greater than 0')
+    .required('Required'),
+  capacity: Yup.number()
+    .moreThan(0, 'Must be greater than 0')
+    .required('Required'),
+  imageUrl: Yup.string().required('Required'),
+});
+
+export default MeetupFormSchema;
