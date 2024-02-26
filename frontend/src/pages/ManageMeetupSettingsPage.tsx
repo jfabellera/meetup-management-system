21a996d3f3961954700a664ce8390e87c836a7bc
Jan-Felix Abellera <jfabellera@gmail.com>
2024-02-26 00:03:25 -0600

    Add form elements and logic

--- frontend/src/pages/ManageMeetupSettingsPage.tsx
+++ frontend/src/pages/ManageMeetupSettingsPage.tsx
@@ -1,46 +1,74 @@
 import {
   Box,
   Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
   Heading,
   HStack,
   Input,
   Spacer,
   Text,
   useBoolean,
-  type BoxProps,
+  type InputProps,
 } from '@chakra-ui/react';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { useFormik } from 'formik';
+import { useEffect } from 'react';
 import { FiEdit } from 'react-icons/fi';
 import { useParams } from 'react-router-dom';
 import { useGetMeetupQuery } from '../store/meetupSlice';
+import MeetupFormSchema from '../util/schemas/MeetupFormSchema';
 
 dayjs.extend(customParseFormat);
 
-interface FieldDisplayProps extends BoxProps {
+interface FieldDisplayProps extends InputProps {
   name: string;
-  value: string | number | null | undefined;
+  value: string | number | undefined;
+  id: string;
+  errorMessage: string | undefined;
   editable: boolean;
 }
 
 const FieldDisplay = ({
   name,
   value,
+  id,
+  type,
+  isInvalid,
+  onChange,
+  onBlur,
+  errorMessage,
   editable,
   ...rest
 }: FieldDisplayProps): JSX.Element => {
   return (
-    <Box paddingY={'0.5rem'} {...rest}>
-      <Text fontWeight={'semibold'}>{name}</Text>
-      {editable ? (
-        <Input
-          variant={'outline'}
-          defaultValue={value?.toString()}
-          maxWidth={'sm'}
-        />
-      ) : (
-        <Text textColor={'blackAlpha.700'}>{value ?? 'N/A'}</Text>
-      )}
+    <Box paddingY={'0.5rem'} maxWidth={'sm'} {...rest}>
+      <FormControl id={id} isInvalid={isInvalid} minWidth={0}>
+        <FormLabel noOfLines={1} marginBottom={'0.2rem'}>
+          {name}
+        </FormLabel>
+        {editable ? (
+          <>
+            <Input
+              type={type}
+              name={id}
+              onChange={onChange}
+              onBlur={onBlur}
+              defaultValue={value}
+            />
+
+            <FormErrorMessage justifyContent={'right'}>
+              {errorMessage}
+            </FormErrorMessage>
+          </>
+        ) : (
+          <>
+            <Text textColor={'blackAlpha.700'}>{value ?? 'N/A'}</Text>
+          </>
+        )}
+      </FormControl>
     </Box>
   );
 };
@@ -49,6 +77,42 @@ const ManageMeetupSettingsPage = (): JSX.Element => {
   const { meetupId } = useParams();
   const { data: meetup } = useGetMeetupQuery(parseInt(meetupId ?? '0'));
   const [editable, setEditable] = useBoolean(false);
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      date: '',
+      startTime: '',
+      address: '',
+      duration: 0,
+      capacity: 0,
+      imageUrl: '',
+    },
+    onSubmit: () => {
+      // TODO(jan): implement
+    },
+    validationSchema: MeetupFormSchema,
+    validateOnMount: true,
+  });
+
+  useEffect(() => {
+    formik.resetForm({
+      values: {
+        name: meetup?.name ?? '',
+        date: dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format('YYYY-MM-DD'),
+        startTime: dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format('hh:mm'),
+        address: meetup?.location.full_address ?? '',
+        duration: meetup?.duration_hours ?? 0,
+        capacity: meetup?.tickets?.total ?? 0,
+        imageUrl: meetup?.image_url ?? '',
+      },
+    });
+  }, [meetup]);
+
+  useEffect(() => {
+    if (!editable) {
+      formik.resetForm();
+    }
+  }, [editable]);
 
   return (
     <Box
@@ -71,38 +135,96 @@ const ManageMeetupSettingsPage = (): JSX.Element => {
           Edit
         </Button>
       </HStack>
-      <FieldDisplay
-        name={'Meetup Name'}
-        value={meetup?.name}
-        editable={editable}
-      />
-      <FieldDisplay
-        name={'Date'}
-        value={dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format(
-          'MMMM DD, YYYY',
-        )}
-        editable={editable}
-      />
-      <FieldDisplay
-        name={'Start Time'}
-        value={dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format('h:mm A')}
-        editable={editable}
-      />
-      <FieldDisplay
-        name={'Address'}
-        value={meetup?.location.full_address}
-        editable={editable}
-      />
-      <FieldDisplay
-        name={'Duration (hours)'}
-        value={meetup?.duration_hours}
-        editable={editable}
-      />
-      <FieldDisplay
-        name={'Capacity'}
-        value={meetup?.tickets?.total}
-        editable={editable}
-      />
+      <form onSubmit={formik.handleSubmit} noValidate>
+        <FieldDisplay
+          name={'Meetup Name'}
+          value={meetup?.name}
+          editable={editable}
+          id={'name'}
+          type={'text'}
+          isInvalid={formik.errors.name != null && formik.touched.name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          errorMessage={formik.errors.name}
+        />
+        <FieldDisplay
+          name={'Date'}
+          value={dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format(
+            'YYYY-MM-DD',
+          )}
+          editable={editable}
+          id={'date'}
+          type={'date'}
+          isInvalid={formik.errors.date != null && formik.touched.date}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          errorMessage={formik.errors.date}
+        />
+        <FieldDisplay
+          name={'Start Time'}
+          value={dayjs(meetup?.date, 'YYYY-MM-DDTHH:mm:ss').format('HH:mm')}
+          editable={editable}
+          id={'startTime'}
+          type={'time'}
+          isInvalid={
+            formik.errors.startTime != null && formik.touched.startTime
+          }
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          errorMessage={formik.errors.startTime}
+        />
+        <FieldDisplay
+          name={'Address'}
+          value={meetup?.location.full_address}
+          editable={editable}
+          id={'address'}
+          type={'text'}
+          isInvalid={formik.errors.address != null && formik.touched.address}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          errorMessage={formik.errors.address}
+        />
+        <FieldDisplay
+          name={'Duration (hours)'}
+          value={meetup?.duration_hours}
+          editable={editable}
+          id={'duration'}
+          type={'number'}
+          isInvalid={formik.errors.duration != null && formik.touched.duration}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          errorMessage={formik.errors.duration}
+        />
+        <FieldDisplay
+          name={'Capacity'}
+          value={meetup?.tickets?.total}
+          editable={editable}
+          id={'capacity'}
+          type={'number'}
+          isInvalid={formik.errors.capacity != null && formik.touched.capacity}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          errorMessage={formik.errors.capacity}
+        />
+        {editable ? (
+          <HStack>
+            <Spacer />
+            <Button
+              type="submit"
+              loadingText="Save"
+              disabled={!formik.isValid}
+              size="lg"
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{
+                bg: 'blue.500',
+              }}
+            >
+              Save
+            </Button>
+          </HStack>
+        ) : null}
+      </form>
     </Box>
   );
 };
