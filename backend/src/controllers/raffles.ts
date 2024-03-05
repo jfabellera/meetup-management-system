39925efae6f27aa1536ac56200783aa445f7ef49
Jan-Felix Abellera <jfabellera@gmail.com>
2024-03-04 23:58:27 -0600

    Add endpoint to generate random raffle winner from checked in attendees

---
+++ backend/src/controllers/raffles.ts
@@ -0,0 +1,48 @@
+import { type Request, type Response } from 'express';
+import { MoreThan, Raw } from 'typeorm';
+import { type Meetup } from '../entity/Meetup';
+import { Ticket } from '../entity/Ticket';
+import { type RaffleWinnerResponse } from '../interfaces/rafflesInterfaces';
+import { generateRandomNumber } from '../util/math';
+
+export const rollRaffleWinner = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const meetup = res.locals.meetup as Meetup;
+
+  // Get checked in tickets with raffle entries
+  const tickets = await Ticket.find({
+    relations: {
+      user: true,
+    },
+    where: {
+      meetup: {
+        id: meetup.id,
+      },
+      is_checked_in: true,
+      raffle_entries: MoreThan(1),
+      raffle_wins: Raw((wins) => `${wins} < raffle_entries`),
+    },
+    select: {
+      id: true,
+      user: {
+        nick_name: true,
+      },
+    },
+  });
+
+  if (tickets.length > 0) {
+    // Randomize winner
+    const winnerIndex = generateRandomNumber(0, tickets.length - 1);
+    const winnerTicket = tickets[winnerIndex];
+    const response: RaffleWinnerResponse = {
+      ticketId: winnerTicket.id,
+      displayName: winnerTicket.user.nick_name,
+    };
+
+    return res.status(200).json(response);
+  }
+
+  return res.status(200).json({});
+};
--- backend/src/routes/meetups.ts
+++ backend/src/routes/meetups.ts
@@ -7,37 +7,50 @@ import {
   getMeetupAttendees,
   updateMeetup,
 } from '../controllers/meetups';
+import { rollRaffleWinner } from '../controllers/raffles';
 import { createTicket } from '../controllers/tickets';
 import { authChecker, Rule } from '../middleware/authChecker';
 
 const router = express.Router();
 
 router.get('/', getAllMeetups as RequestHandler);
+
 router.get('/:meetup_id', getMeetup as RequestHandler);
+
 router.post(
   '/',
   authChecker([Rule.requireOrganizer]) as RequestHandler,
   createMeetup as RequestHandler
 );
+
 router.put(
   '/:meetup_id',
   authChecker([Rule.requireOrganizer]) as RequestHandler,
   updateMeetup as RequestHandler
 );
+
 router.delete(
   '/:meetup_id',
   authChecker([Rule.requireOrganizer]) as RequestHandler,
   deleteMeetup as RequestHandler
 );
+
 router.post(
   '/:meetup_id/rsvp',
   authChecker([Rule.ignoreMeetupOrganizer]) as RequestHandler,
   createTicket as RequestHandler
 );
+
 router.get(
   '/:meetup_id/attendees',
   authChecker([Rule.requireOrganizer]) as RequestHandler,
   getMeetupAttendees as RequestHandler
+);
+
+router.post(
+  '/:meetup_id/raffle',
+  authChecker([Rule.requireOrganizer]) as RequestHandler,
+  rollRaffleWinner as RequestHandler
 );
 
 export default router;
