8b128590c4072d92dfb4739c61420fb2eb4319bb
Jan-Felix Abellera <jfabellera@gmail.com>
2024-04-27 05:10:48 -0500

    Move informational cards into grid

--- frontend/src/pages/ManageMeetupHomePage.tsx
+++ frontend/src/pages/ManageMeetupHomePage.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Grid, GridItem, HStack } from '@chakra-ui/react';
+import { Button, Flex, Grid, GridItem } from '@chakra-ui/react';
 import dayjs from 'dayjs';
 import isBetween from 'dayjs/plugin/isBetween';
 import { useMemo } from 'react';
@@ -41,66 +41,70 @@ const ManageMeetupHomePage = (): JSX.Element => {
   return (
     <Flex margin={'1rem'} justify={'center'} direction={'column'}>
       {meetup != null && attendees != null ? (
-        <HStack width={'100%'} spacing={3} maxWidth={'800px'}>
-          {/* Show how many have checked in if meetup is currently happening, otherwise show how many have signed up */}
-          <FractionCard
-            numerator={
-              isHappeningNow
-                ? attendees.filter((attendee) => attendee.is_checked_in).length
-                : (meetup.tickets?.total ?? 0) -
-                  (meetup.tickets?.available ?? 0)
-            }
-            denominator={
-              isHappeningNow ? attendees.length : meetup.tickets?.total ?? 0
-            }
-            label={isHappeningNow ? 'checked in' : 'signed up'}
-            onClick={() => {
-              navigate(
-                `/meetup/${meetupId}/manage/${
-                  isHappeningNow ? 'checkin' : 'attendees'
-                }`
-              );
-            }}
-            width={'50%'}
-            _hover={{
-              cursor: 'pointer',
-            }}
-          />
-          {/* Show detailed countdown until meetup end if meetup is currently happening, otherwise show relative time until start of meetup or end of meetup */}
-          <CountDownCard
-            date={
-              hasStarted || hasEnded
-                ? dayjs(meetup.date).add(meetup.duration_hours ?? 0, 'hours')
-                : dayjs(meetup.date)
-            }
-            futureText={!hasStarted ? 'left' : 'until end'}
-            pastText={'ago'}
-            width={'50%'}
-            simple={!isHappeningNow}
-          />
-        </HStack>
-      ) : null}
+        <Grid
+          templateColumns={'repeat(2, 1fr)'}
+          templateRows={'repeat(2, 100px)'}
+          gap={4}
+          width={'100%'}
+          paddingY={'0.75rem'}
+        >
+          <GridItem colSpan={1}>
+            {/* Show how many have checked in if meetup is currently happening, otherwise show how many have signed up */}
+            <FractionCard
+              numerator={
+                isHappeningNow
+                  ? attendees.filter((attendee) => attendee.is_checked_in)
+                      .length
+                  : (meetup.tickets?.total ?? 0) -
+                    (meetup.tickets?.available ?? 0)
+              }
+              denominator={
+                isHappeningNow ? attendees.length : meetup.tickets?.total ?? 0
+              }
+              label={isHappeningNow ? 'checked in' : 'signed up'}
+              onClick={() => {
+                navigate(
+                  `/meetup/${meetupId}/manage/${
+                    isHappeningNow ? 'checkin' : 'attendees'
+                  }`
+                );
+              }}
+              width={'100%'}
+              _hover={{
+                cursor: 'pointer',
+              }}
+            />
+          </GridItem>
 
-      <Grid
-        templateColumns={'repeat(2, 1fr)'}
-        templateRows={'repeat(2, 100px)'}
-        gap={4}
-        width={'100%'}
-        paddingY={'0.75rem'}
-      >
-        <GridItem colSpan={2}>
-          <Button
-            colorScheme={'blackAlpha'}
-            width={'100%'}
-            height={'100%'}
-            onClick={() => {
-              navigate(`/meetup/${meetupId}/manage/raffle`);
-            }}
-          >
-            Raffles
-          </Button>
-        </GridItem>
-      </Grid>
+          <GridItem colSpan={1}>
+            {/* Show detailed countdown until meetup end if meetup is currently happening, otherwise show relative time until start of meetup or end of meetup */}
+            <CountDownCard
+              date={
+                hasStarted || hasEnded
+                  ? dayjs(meetup.date).add(meetup.duration_hours ?? 0, 'hours')
+                  : dayjs(meetup.date)
+              }
+              futureText={!hasStarted ? 'left' : 'until end'}
+              pastText={'ago'}
+              width={'100%'}
+              simple={!isHappeningNow}
+            />
+          </GridItem>
+
+          <GridItem colSpan={2}>
+            <Button
+              colorScheme={'blackAlpha'}
+              width={'100%'}
+              height={'100%'}
+              onClick={() => {
+                navigate(`/meetup/${meetupId}/manage/raffle`);
+              }}
+            >
+              Raffles
+            </Button>
+          </GridItem>
+        </Grid>
+      ) : null}
     </Flex>
   );
 };
