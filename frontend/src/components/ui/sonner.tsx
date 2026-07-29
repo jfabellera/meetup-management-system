93035098301861efcee9100710dce38cf472ca05
Jan-Felix Abellera <jfabellera@gmail.com>
2026-07-28 19:46:19 -0500

    Improve toast location on screens with bottom nav bar

--- frontend/src/components/ui/sonner.tsx
+++ frontend/src/components/ui/sonner.tsx
@@ -16,6 +16,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps['theme']}
+      closeButton
       className="toaster group"
       icons={{
         success: <CircleCheckIcon className="size-4" />,
